325546d261a34d825cb605140c32bb750c56d3ad
Omar <mourra24@gmail.com>
2023-01-31 20:37:58 +0200

    started working on the hero page and started expereminting
    added 2 sections using boostrap and still adding more

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,13 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <div>
+      Hello and welcome to my Portofolio
+    </div>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
   ,
 )
