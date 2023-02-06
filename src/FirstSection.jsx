e43dd8eba4f8ee53fcf0495a2cd0c59e641009bf
Omar <mourra24@gmail.com>
2023-02-06 19:52:25 +0200

    updated font and the main page

--- src/FirstSection.jsx
+++ src/FirstSection.jsx
@@ -5,9 +5,25 @@ import Image from 'react-bootstrap/Image'
 function FirstSection() {
 
     return (
-        <>
-                    <Image src='./pixelball-export.gif' className='img_first' />
-        </>
+        <div className='firstsection'>
+            <h1 style={{padding:'2vmax'}}>Hello, Adventurer welcome to my profile.</h1>
+
+            <p>
+                My name is <b style={{fontSize:'1.3vmax'}}>Omar Yousef</b> a third-year Computer Engineering Student at Ain shams University computer graphics major.
+                <br />
+                    From an early age, I fell in love with video games and SFX effects in animations and movies.
+                <br/>
+                    So I decided to embark on my odyssey and started learning whatever I can put my hands on hoping that it will help
+                    me to achieve my dream one day which is creating my own game on the same level as the<br/> <b>" The Legend Of Zelda "</b><br/> series.
+                <br />
+                    as for my favorite quote :
+                <br />
+                <b className='quote' >
+                    `` A jack of all trades is a master of none, but oftentimes better than a master of one. ``
+                </b>
+            </p>
+            <Image src='./bwselfie2.png' className='img_first' />
+        </div>
     )
 }
 
--- src/Navigationbar.jsx
+++ src/Navigationbar.jsx
@@ -3,7 +3,7 @@ import { Button } from "bootstrap"
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
 import { SidebarMenuNav } from "react-bootstrap-sidebar-menu"
 
-import { BsGithub, BsYoutube, BsInstagram } from 'react-icons/bs'
+import { BsGithub, BsYoutube, BsInstagram , BsLinkedin } from 'react-icons/bs'
 
 
 function Navigationbar() {
@@ -24,10 +24,10 @@ function Navigationbar() {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="me-auto" style={{padding:'3vmax'}}>
               <Nav.Item >
                 {/* <Nav.Link href="/home">About</Nav.Link> */}
-                Hello adventurer welcome to my profile for now the site is under construction until then you can check my other profiles on other platforms
+                Unfortunately site is under construction<br/>🚧👷🚧<br/>until then you can check my other profiles on other platforms
               </Nav.Item>
               {/* <Nav.Item>
                 <Nav.Link eventKey="link-1">Projects</Nav.Link>
@@ -36,15 +36,18 @@ function Navigationbar() {
                 <Nav.Link eventKey="link-2">Contact</Nav.Link>
               </Nav.Item> */}
               <Nav.Item>
-                <div eventKey="link-2" className="icons">
-                  <a href="https://github.com/mourra950">
-                    <BsGithub className="icon Github" />
+                <div  className="icons">
+                  <a className=" Github" href="https://github.com/mourra950">
+                    <BsGithub className="icon " />
                   </a>
-                  <a href="https://www.youtube.com/@mourra9598/videos">
-                    <BsYoutube className="icon Youtube" />
+                  <a className=" Youtube" href="https://www.youtube.com/@mourra9598/videos">
+                    <BsYoutube className="icon " />
                   </a>
-                  <a href="https://www.instagram.com/mourra_odyssy/">
-                    <BsInstagram className="icon Insta" />
+                  <a className=" Insta" href="https://www.instagram.com/mourra_odyssy/">
+                    <BsInstagram className="icon " />
+                  </a>
+                  <a  className=" Linkdin" href="https://www.linkedin.com/in/mourra950/">
+                    <BsLinkedin className="icon " />
                   </a>
                 </div>
               </Nav.Item>
