41b7753cc35a7145e7454b75c7f4ab467d60674c
Omar Mohamed Yousef <mourra24@gmail.com>
2023-07-15 09:12:14 +0300

    updated

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -89,7 +89,35 @@ function Project() {
                                 </Col>
                             </Row>
                             <Divider />
-
+                            <div >
+                                <Title className='SE'>
+                                    Car Distributed Systems Game (Serene Track)
+                                </Title>
+                                <Paragraph className='SE' style={{ lineHeight: '36px' }}>
+                                    I worked on this project with the help of <a href='https://github.com/OmarElbanna'>Eng/Omar Elbanna</a>.We created a multiplayer racing car game where players compete in real-time, even if their connection is lost or they experience a crash. We focused on making the game robust and distributed across multiple clients and servers. To achieve this, we used basic libraries and coded the system ourselves, allowing us to learn and improve our socket programming skills.
+                                </Paragraph>
+                                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                                    <Image
+                                        src="./photos/project/Node_Distribution.svg"
+                                    />
+                                    <p style={{ textAlign: 'center', margin: '10px' }}>
+                                        <b>Figure 1 :</b> Distributed system project structure</p>
+                                </div>
+                                <Paragraph className='SE' style={{ lineHeight: '36px' }}>
+                                    Our project incorporates various components to deliver a captivating multiplayer gaming experience. It includes a Unity game instance developed with the Unity game engine, a Python proxy server that acts as an intermediary between the Unity game and the dedicated game server, a Node.js-based dedicated game server deployed on EC2 to handle game logic and real-time interactions using the Socket.IO library, a MongoDB server for storing game information and updates, a Node.js data server that retrieves data from MongoDB and provides a REST API for the React webpage, and finally, a React webpage that showcases the project, utilizing data from the Node.js data server. This architecture ensures seamless communication among the Unity game instance, Python proxy server, dedicated game server, and web components, resulting in an engaging and dynamic multiplayer gaming experience.
+                                </Paragraph>
+                            </div>
+                            <Title className='SE'>
+                                Links
+                            </Title>
+                            <Paragraph className='SE' style={{ lineHeight: '36px' }}>
+                                <ul>
+                                    <li>Game Page : <a href='https://mourra950.github.io/Distributed-Car-Game-Site/'>Live page [Github pages]</a></li>
+                                    <li>Github Repo : <a href='https://github.com/mourra950/Distrubited-System-Racing-Cars'>Main Repo</a></li>
+                                    <li>Youtube Trailer : <a href='https://www.youtube.com/watch?v=XfoahjVGTwg&ab_channel=mourra95'>Youtube Video</a></li>
+                                </ul>
+                            </Paragraph>
+                            <Divider />
                             <div >
                                 <Title className='SE'>
                                     Omar Yousef Portofolio
@@ -102,7 +130,7 @@ function Project() {
                                         <source src="./Videos/Porfolio.mp4" type="video/mp4" />
                                     </video>
                                     <p style={{ textAlign: 'center', margin: '10px' }}>
-                                        <b>Figure 1 :</b> My Portfolio version 2 made in senior 1 using Reactjs</p>
+                                        <b>Figure 2 :</b> My Portfolio version 2 made in senior 1 using Reactjs</p>
                                 </div>
                                 <Paragraph className='SE' style={{ lineHeight: '36px' }}>
                                     My portfolio website has undergone a journey of evolution and transformation. Initially, it was a single page crafted with the MUI library, but I realized that to truly showcase my skills and creativity, I needed more. So I delved into the React Router Dom and expanded the site to incorporate multiple pages, injecting a fresh dynamic into the site's design. The next step was to replace the MUI library with the antd library, which allowed me to experiment more freely with CSS, bringing a unique flair to the site's appearance. To further enhance its aesthetic appeal, I added custom libraries for animation, giving the site a touch of whimsy and playfulness. As my passion for design grew, I began incorporating SASS and adding captivating glitching effects, giving my portfolio a distinct and memorable edge. Creating this website has been an enjoyable and rewarding experience, and I am eager to continue pushing the boundaries of design to create something truly remarkable.
@@ -130,7 +158,7 @@ function Project() {
                                         <source src="./Videos/Room.mp4" type="video/mp4" />
                                     </video>
                                     <p style={{ textAlign: 'center', margin: '10px' }}>
-                                        <b>Figure 2 : </b>My Room made in blender and integrated using react three fiber</p>
+                                        <b>Figure 3 : </b>My Room made in blender and integrated using react three fiber</p>
                                 </div>
                                 <Paragraph className='SE' style={{ lineHeight: '36px' }}>
                                     My journey into the world of 3D design began a year ago when I first discovered Blender. I was captivated by its versatility and power, but I never imagined that I could integrate my 3D models into the web. That was until I began learning Three.js, and subsequently, React Three Fiber. Learning the basics of computer graphics was an essential step in my journey, and it gave me the foundation I needed to create truly remarkable 3D scenes. React Three Fiber made the process of creating scenes so much more accessible, and it opened up new possibilities that I had never considered before. The journey was challenging, but it was also highly enjoyable, and I am proud that I faced it head-on. I am excited to see where this newfound passion for 3D design will take me in the future.         </Paragraph>
@@ -157,7 +185,7 @@ function Project() {
                                         <source src="./Videos/Rover.mp4" type="video/mp4" />
                                     </video>
                                     <p style={{ textAlign: 'center', margin: '10px' }}>
-                                        <b>Figure 3 :</b> Rover in action </p>
+                                        <b>Figure 4 :</b> Rover in action </p>
                                 </div>
                                 <Paragraph className='SE' style={{ lineHeight: '36px' }}>
                                     As a university project for a computer vision course, my project involved implementing flawless execution, collision avoidance, and real-time mapping of the surrounding world. I am proud to say that the project was a success, and we were able to achieve a mapping accuracy of 95% with 90% precision, which resulted in top marks for the project.
@@ -182,6 +210,35 @@ function Project() {
                                     I have a passion for animation and cartoons, and lately, I've been honing my skills in Blender. I've been creating 3D models of my favorite characters and scenes, and I'm proud to share my progress on my <a href='https://www.instagram.com/mourra_odyssy/'>Instagram page</a>. If you're interested, feel free to check it out!
                                 </Paragraph>
                                 <Carousel>
+                                    <Carousel.Item>
+                                        <video disablePictureInPicture loop autoPlay width="100%">
+                                            <source src="./photos/project/animation.mp4" type="video/mp4" />
+                                        </video>
+                                        <Carousel.Caption>
+                                            <h3>Lego</h3>
+                                            <p>My first flawless animation</p>
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <Image
+                                            src="./photos/project/emmet.jpg"
+                                            alt="First slide"
+                                        />
+                                        <Carousel.Caption>
+                                            <h3>Emmet</h3>
+                                            <p>Lego Movie</p>
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <Image
+                                            src="./photos/project/beachph.jpg"
+                                            alt="First slide"
+                                        />
+                                        <Carousel.Caption>
+                                            <h3>Beach</h3>
+                                            <p>Monster Inc.</p>
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
                                     <Carousel.Item>
                                         <Image
                                             src="./photos/project/jake.jpg"
@@ -221,7 +278,8 @@ function Project() {
                                             <h3>Squidward House</h3>
                                             <p>Spongbob Squarepants</p>
                                         </Carousel.Caption>
-                                    </Carousel.Item><Carousel.Item>
+                                    </Carousel.Item>
+                                    <Carousel.Item>
                                         <Image
                                             src="./photos/project/shamshom.jpg"
                                             alt="First slide"
@@ -231,10 +289,11 @@ function Project() {
                                             <p>Spongbob Squarepants</p>
                                         </Carousel.Caption>
                                     </Carousel.Item>
+
                                 </Carousel>
 
                                 <Paragraph className='SE' style={{ lineHeight: '36px' }}>
-                                    I am continuing to refine my 3D modeling skills and delving deeper into the realm of materials and material maps. In the near future, I intend to develop my own assets for use in my game.
+                                    I am continuing to refine my 3D modeling skills and delving deeper into the realm of materials and material maps. In the near future, I intend to make a fully imersive animation.
                                 </Paragraph>
                             </div>
 
