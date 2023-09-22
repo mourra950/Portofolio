a5d8d561e8b1ea1d40036828d18ba534c6f6af3b
Omar Mohamed Yousef <mourra24@gmail.com>
2023-09-22 15:36:33 +0300

    implemented some animation
    using framer motion library and changing the reac-router structure next i will change the layout organization to be more modular

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,14 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import Hero from './Hero.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Main from './pages/FirstPage/indexpage.jsx'
-import About from './pages/About/About.jsx'
-import Experience from './pages/Experience/Experience.jsx'
-import Project from './pages/Project/Project.jsx'
-
-const router = createBrowserRouter([
-  {
-    index: true,
-    path: "/",
-    element: (<Main />),
-  },
-  {
-    path: "contact",
-    element: (<Hero />),
-  },
-  {
-    path: "about",
-    element: (<About />),
-  },
-  {
-    path: "project",
-    element: (<Project />),
-  },
-  {
-    path: "experience",
-    element: (<Experience />),
-  },
-], { basename: '/Portofolio' });
-
+import { App } from './App'
+import {  BrowserRouter, Routes, Route } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <Suspense fallback={<h1>Hello</h1>}>
-      <RouterProvider router={router} />
-    </Suspense>
+    <BrowserRouter basename="/Portofolio">
+      <Routes>
+        <Route path='/*' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </>
 
 )
