3de773d5a023697fb272930c920518b0dc1cf6b4
Omar Mohamed Yousef <mourra24@gmail.com>
2023-09-23 10:45:44 +0300

    animation tweak and added more to experience

--- src/pages/Experience/experiencedata.js
+++ src/pages/Experience/experiencedata.js
@@ -1,9 +1,23 @@
 export const data = [
     {
+        role: 'NOKIA Shadowing Program ',
+        title: 'Network Intern',
+        des: 'Attended sessions about NOKIA and mobile networks infrastructure from CORE, RAN, waves, IP, SDM, 5G,and etc....and shadowed the team working on Orange services and observed the process of troubleshooting issues and bugs during a major system update and observed NOKIA GUI software and how profiles are made and stored and the parameters available.',
+        date: 'Aug 2023 - Sep 2023'
+    }
+    ,
+    {
+        role: 'Game Jam GMTK',
+        title: 'Participant',
+        des: ' Participated in the Game Jam GMTK 2023 with the theme reversed roles and collaborated with a team to create a game where the character switch roles with the villains in a platform actiongame to reach the end of the mountain',
+        date: 'Jul 2023 - Jul 2023'
+    }
+    ,
+    {
         role: 'Google Developer Solution ',
         title: 'Challenger',
         des: 'I contributed to the development of Global Care, a user-friendly website for medical record storage and access, using ReactJS for frontend, database on Firestore, and developed the backend on Firebase as part of the Google Solution Challenge 2023.',
-        date: 'Feb 2023 - Present'
+        date: 'Feb 2023 - Mar 2023'
     }
     ,
     {
@@ -19,31 +33,42 @@ export const data = [
         des: "As an Engineer intern in TV broadcasting, I gained firsthand knowledge of quality assurance, media broadcasting, quality control, production, and satellites, while learning from experienced professionals in the field."
         , date: 'Jun 2022 - Aug 2022'
 
-    },
+    }
+    ,
     {
         role: 'AIESEC',
         title: 'Incoming global teaching project manager',
         des: "I researched and identified teaching opportunities and partnerships, organized preparation seminars for cultural and language adaptation, achieved recognition as the best team member during Q1Q2, assisted with recruitment and onboarding of new members, and acted as a guide to foreigners.",
         date: 'Jan 2022 - Aug 2022'
 
-    },
+    }
+    ,
     {
         role: 'ICPC ',
         title: 'Contestant',
         des: "I attended sessions to learn about computer algorithms and problem-solving using C and C++ for maximum efficiency, applying what I learned with my team in the ICPC competition and achieving a ranking of 17 at Ain Shams University.",
         date: 'Jun 2021 - Aug 2021'
-    },
-    {
-        role: 'Cisco',
-        title: 'IT intern',
-        des: 'Worked for a month as an intern for IT and made some logos for some applications they were working on.',
-        date: ''
-    },
+    }
+    ,
     {
         role: 'SOUL',
         title: 'Marketing Member',
         des: 'University Student activity I was responsible for Graphic designing and video editing of any marketing materials needed for the sponsors or their social platforms but closed shortly because of covid.',
-        date: ''
+        date: 'Nov 2020 - Dec 2020'
+    }
+    ,
+    {
+        role: 'Cisco',
+        title: 'IT intern',
+        des: 'Worked for a month as an intern for IT and made some logos for some applications they were working on.',
+        date: 'Jun 2020 - Jul 2020'
+    }
+    ,
+    {
+        role: ' IGSCE Teacher Assistant',
+        title: 'Math and Chemistry Advanced and Ordinary Level',
+        des: 'Taught weak students , proctored and corrected exams',
+        date: 'Jul 2018 - Mar 2020'
     }
 
 ]
--- src/pages/FirstPage/indexpage.jsx
+++ src/pages/FirstPage/indexpage.jsx
@@ -34,7 +34,7 @@ export default function Main() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 1, delay: 0.3, ease: [0.17, 0.67, 0.83, 0.67] }}
+            transition={{ duration: 1, delay: 0.5, ease: [0.17, 0.67, 0.83, 1] }}
             className='firstpage'>
             <p ref={glitch.ref}  >Omar Yousef</p>
             <div>
